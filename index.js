a632892f90165211441f7d3cfbb77683432d951a
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-04-19 11:55:06 -0500

    Added JSDoc comments

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+/**
+ * Bighook class
+ * @class
+ */
+
 class Bighook {
+  /**
+   * Constructor for Bighook
+   * @param {Function} func - Function to add hooks on to
+   */
+
   constructor(func) {
     this.func = func;
   }
+
+  /**
+   * Add a hook to run before the call
+   * @param {Function} callback - Function to run as hook
+   * @returns {Bighook}
+   */
 
   before(callback) {
     const original = this.func;
@@ -13,6 +29,12 @@ class Bighook {
     return this;
   }
 
+  /**
+   * Add a hook to run after the call
+   * @param {Function} callback - Function to run as hook
+   * @returns {Bighook}
+   */
+
   after(callback) {
     const original = this.func;
     this.func = () => {
@@ -23,6 +45,11 @@ class Bighook {
 
     return this;
   }
+
+  /**
+   * Get the function with hooks attached
+   * @returns {Function}
+   */
 
   function() {
     return () => {
