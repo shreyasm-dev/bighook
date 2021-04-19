ca113d89d32faf1397f7a66efe9004c50a96b536
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-04-19 11:10:36 -0500

    Added before and after methods and updated version

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ class Bighook {
     this.func = func;
   }
 
+  before(callback) {
+    const original = this.func;
+    this.func = () => {
+      callback();
+      return original();
+    };
+
+    return this;
+  }
+
+  after(callback) {
+    const original = this.func;
+    this.func = () => {
+      const returnVal = original();
+      callback();
+      return returnVal;
+    };
+
+    return this;
+  }
+
   function() {
     return () => {
       this.func();
